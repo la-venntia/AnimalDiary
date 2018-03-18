96cba8176d02999277ef2e66428062572c7b76a1
lavenntia <valentina.amanda@tokopedia.com>
2018-03-19 02:49:59 +0700

    new project

---
+++ AnimalDiary/AnimalDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimalDiary
+//
+//  Created by Valentina Widiyanti Amanda on 19/03/18.
+//  Copyright © 2018 VWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
